fe6513e7e4750569306cc3ef0e40bbca442e1fe2
Suvam Swagatam panda <suvampandar@gmail.com>
2023-05-20 12:01:58 +0530

    initial commit with all dependancy

---
+++ src/AllRoutes/AllRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import { Dashboard } from '@mui/icons-material'
+
+const AllRoutes = () => {
+  return (
+    <>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default AllRoutes
---
+++ src/Components/AssignTodoToUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AssignTodoToUser = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AssignTodoToUser
